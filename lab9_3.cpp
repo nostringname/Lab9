6970271d0e8128c786200534614951619f423e30
notmyname <qwrwadsa45sda56as4d561xa56x45csad@gmail.com>
2022-12-23 16:09:50 +0700

    Done

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,69 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age;
+    int bounty;
+    int height;
+    string character;
+
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age <= 25) {
+
+        cout << "Enter your height: ";
+        cin >> height;
+
+        if (height < 100) {
+            character = "Chopper";
+        }
+        else if (height < 180) {
+            character = "Usopp";
+        }
+        else {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+
+            if (bounty > 1.1e9) {
+                character = "Zoro";
+            }
+            else {
+                character = "Sanji";
+            }
+
+        }
+
+
+    }
+
+    else {
+
+        if (age <= 60) {
+
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+
+            if (bounty > 5e8) {
+                character = "Jinbe";
+            }
+            else {
+                character = "Franky";
+            }
+
+
+        }
+
+        else {
+
+            character = "Brook";
+
+        }
+
+
+    }
+
+cout << "Your character = "<< character;
+}
